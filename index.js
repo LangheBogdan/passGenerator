9d0c934e6e35bea3b067c8fbcc799a2fc2e1c2a3
Bogdan Langhe <bogdanlanghe@gmail.com>
2023-11-20 10:01:19 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,114 @@
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+let passOneEl = document.getElementById("pass-one-el"); //?
+let passTwoEl = document.getElementById("pass-two-el"); //?
+let generatePasswordEl = document.getElementById("generate-password-el"); //?
+function generatePassword(length) {
+  let password = "";
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    password += characters[randomIndex];
+  }
+  return password;
+}
+
+generatePasswordEl.addEventListener("click", function () {
+  const length = document.getElementById("passwordLength").value || 15;
+  const password1 = generatePassword(length);
+  const password2 = generatePassword(length);
+  passOneEl.textContent = password1;
+  passTwoEl.textContent = password2;
+});
+
+// Generate two 15-character passwords
